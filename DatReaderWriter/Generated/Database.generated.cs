849977c41c4fe267bdc781337232aa1de1840c6c
trevis <trevis@haxit.org>
2025-04-17 16:42:04 -0600

    Add LanguageInfo DBObj, add LocalDatabase wrapper

---
+++ DatReaderWriter/Generated/Database.generated.cs
@@ -0,0 +1,26 @@
+//!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!//
+//                                                            //
+//                          WARNING                           //
+//                                                            //
+//           DO NOT MAKE LOCAL CHANGES TO THIS FILE           //
+//               EDIT THE .tt TEMPLATE INSTEAD                //
+//                                                            //
+//!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!//
+
+
+using System.Numerics;
+using System.IO;
+using System.Linq;
+using System.Collections.Generic;
+using DatReaderWriter.Enums;
+using DatReaderWriter.Types;
+using DatReaderWriter.Lib;
+using DatReaderWriter.Lib.Attributes;
+using DatReaderWriter.Lib.IO;
+using DatReaderWriter.DBObjs;
+
+namespace DatReaderWriter {
+    public partial class Database {
+        
+    }
+}
